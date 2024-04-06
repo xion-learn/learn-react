9e55807512e44123f28dd61247abd730d84615ca
xionkq <3030199850@qq.com>
2024-04-06 18:35:14 +0800

    add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      button
+    </button>
+  )
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>欢迎</h1>
+      <MyButton></MyButton>
+    </div>
+  )
+}
