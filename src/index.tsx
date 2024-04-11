4861a3b4e267af1d0666bcb9ec4eecf17cb86fa1
xionkq <3030199850@qq.com>
2024-04-11 22:31:31 +0800

    use typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App/ProductList.tsx'
+import App from './App/ProductList'
 import './styles.css'
 
 const root = createRoot(document.getElementById('root'))
