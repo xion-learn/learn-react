ca003522a60d5355aa12a4f27d283d40e53521ff
xionkq <3030199850@qq.com>
2024-04-19 22:12:20 +0800

    use Context

--- src/Doc/ManagingState.tsx
+++ src/Doc/ManagingState.tsx
@@ -1,4 +1,62 @@
-import { useState, useReducer } from 'react';
+import { useState, useReducer, createContext, useContext } from 'react';
+
+const LevelContext = createContext(0);
+
+export default function Page() {
+  return (
+    <Section>
+      <Heading>主标题</Heading>
+      <Section>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Section>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Section>
+            <Heading>子子标题</Heading>
+            <Heading>子子标题</Heading>
+            <Heading>子子标题</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
+}
+
+function Section({ children }) {
+  const level = useContext(LevelContext);
+  return (
+    <section className="section">
+      <LevelContext.Provider value={level + 1}>
+        {children}
+      </LevelContext.Provider>
+    </section>
+  );
+}
+
+function Heading({ children }) {
+  const level = useContext(LevelContext);
+  switch (level) {
+    case 0:
+      throw Error('Heading 必须在 Section 内部！');
+    case 1:
+      return <h1>{children}</h1>;
+    case 2:
+      return <h2>{children}</h2>;
+    case 3:
+      return <h3>{children}</h3>;
+    case 4:
+      return <h4>{children}</h4>;
+    case 5:
+      return <h5>{children}</h5>;
+    case 6:
+      return <h6>{children}</h6>;
+    default:
+      throw Error('未知的 level：' + level);
+  }
+}
 
 function TaskApp() {
   const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
@@ -75,7 +133,7 @@ const initialTasks = [
   {id: 2, text: '打卡列侬墙', done: false}
 ];
 
-export default function App() {
+function App() {
   const [isFancy, setIsFancy] = useState(false);
   if (isFancy) {
     return (
