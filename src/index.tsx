d46553097a94050bddb7e95f0851a2a8d4f17b8d
xionkq <3030199850@qq.com>
2024-04-20 14:11:34 +0800

    use Ref

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App/ProductList'
-import App from './Doc/ManagingState'
+import App from './Doc/EscapeHatches'
 import './styles.css'
 
 const root = createRoot(document.getElementById('root'))
